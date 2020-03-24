2cbe2255d4f99ce34df770ed7cde6541b0f0093f
danyibalazs <danyi.balage@gmail.com>
2020-03-24 17:18:41 +0000

    finish mvp

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,22 @@
+import java.util.ArrayList;
+
 public class Library {
+
+    private ArrayList<Book> books;
+    private int capacity;
+
+    public Library(int capacity){
+        this.books = new ArrayList<Book>();
+        this.capacity = capacity;
+    }
+
+    public int getBooks() {
+        return this.books.size();
+    }
+
+    public void addBook(Book book) {
+        if (this.capacity > getBooks()) {
+            this.books.add(book);
+        }
+    }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,3 +1,49 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+
+    private Library library;
+
+    private Book book1;
+    private Book book2;
+    private Book book3;
+
+
+
+    @Before
+    public void before(){
+        this.library = new Library(100);
+
+        this.book1 = new Book("title1", "author1", "crime");
+        this.book2 = new Book("title2", "author2", "horror");
+        this.book3 = new Book("title3", "author3", "thriller");
+    }
+
+    @Test
+    public void libraryStartsEmpty(){
+        assertEquals(0, library.getBooks());
+    }
+
+    @Test
+    public void testAddBookWithEnoughCapacity(){
+        library.addBook(book1);
+        library.addBook(book2);
+        library.addBook(book3);
+        assertEquals(3, library.getBooks());
+    }
+
+    @Test
+    public void testAddBookWithoutEnoughCapacity(){
+        Library smallLibrary = new Library(2);
+        smallLibrary.addBook(book1);
+        smallLibrary.addBook(book2);
+        smallLibrary.addBook(book3);
+        assertEquals(2, smallLibrary.getBooks());
+    }
 
 }
